74fc6bb6bac0c7ff0a338f3054124caa4420d5db
chengdajing <2328837890@qq.com>
2020-01-04 20:46:57 +0800

    mine

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,22 +1,59 @@
 <template>
   <div>
+
     <view class="userinfo">
     <view class="userinfo-avatar">
     <open-data type="userAvatarUrl"></open-data>
     </view>
     <open-data type="userNickName"></open-data>
     </view>
+    <i-panel title="这是什么神仙穿搭，我要推荐！！！">
+      <i-input :value="name" @change="changeName($event)" title="名称" autofocus placeholder="请输入名称" maxlength="20" />
+      
+      <i-input :value="reason" @change="changeReason($event)" title="推荐理由" placeholder="请输入推荐理由" maxlength="50" />
+    </i-panel>
+    <i-button @click="handleClick" type="success" :ghost="true" shape="circle" size="small" >我要推荐</i-button>
+    
   </div>
+   
 </template>
 
 <script>
+
 export default {
   data () {
     return {
+      name:"",
+      
+      reason:""
     }
   },
 
   methods: {
+    changeName (event) {
+      this.name = event.mp.detail.detail.value
+    },
+    changeReason (event) {
+      this.reason = event.mp.detail.detail.value
+    },
+    
+    handleClick () {
+      if (this.name && this.reason ) {
+        wx.showToast({
+          title: '推荐了' + this.name,
+          icon: 'warning',
+          duration: 2000
+        })
+        // TODO:将推荐数据提交到云数据库
+      } else {
+        wx.showToast({
+          title: '信息不完整',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    }
+   
   },
 
   created () {
@@ -29,7 +66,7 @@ export default {
   position: relative;
   width: 750rpx;
   height: 320rpx;
-  color: #666;
+  color:#121212;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -45,5 +82,10 @@ export default {
   border-radius: 50%;
   border: 2px solid #fff;
   box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+}
+.tips {
+  padding: 20pt;
+  font-size: 10pt;
+  color:lightblue;
 }
 </style>
